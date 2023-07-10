0475833236b196c805c143285a07c8324eb9c186
Beppe Catanese <gcatanese@yahoo.com>
2023-07-10 23:58:07 +0200

    Open extension

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,6 @@
 // service worker
+
+// Allows users to open the side panel by clicking on the action toolbar icon
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
